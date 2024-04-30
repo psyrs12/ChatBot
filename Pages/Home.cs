d2b5fa4747b4eeb4db03c9fe50532978c406112f
Rafal Szponarski <rafalszponarski@zustech.com>
2024-04-30 05:35:57 +0100

    Add project files.

---
+++ Pages/Home.cs
@@ -0,0 +1,23 @@
+﻿using AutoGen.Core;
+using AutoGen.LMStudio;
+
+namespace ChatBot.Pages
+{
+    public partial class Home
+    {
+        private string? inputValue { get; set; }
+        private List<string> messages { get; set; } = new List<string>();
+
+        public async Task Chat(string? content)
+        {
+            if (content == null) { return; }
+            var config = new LMStudioConfig("localhost", 1234);
+            var lmAgent = new LMStudioAgent("Bobby", config: config)
+                .RegisterPrintMessage();
+            IMessage response = await lmAgent.SendAsync(content);
+            messages.Add("User:   " + inputValue ?? string.Empty);
+            messages.Add("Bobby:  " + response.GetContent());
+            inputValue = null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿#ChatBot
+Customer service bot that takes care of troubleshooting customers problems online saving staff time and better service customers needs
+
+#Setup
+- Download LM Studio (https://lmstudio.ai/)
+- Import Model
+- Run local server
+- Run Code
